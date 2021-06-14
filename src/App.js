a009afd56024af0b7d7ee1c6a8bbdb1b97ab9484
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-06-14 12:12:20 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import pokemons from './data';
 import Pokedex from './Pokedex';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import PokemonDetails from './PokemonDetails';
+import About from './About';
 
 function App() {
   return (
@@ -12,10 +13,12 @@ function App() {
         <h1> Pokedex </h1>
         <nav>
           <Link to="/"> Home </Link>
+          <Link to="/About"> About </Link>
         </nav>
       </div>
       <Switch>
         <Route exact path="/" render={() => (<Pokedex pokemons={pokemons} />)}/>
+        <Route  path="/About" component={About}/>
         <Route path="/pokemons/:id" render={(props) => <PokemonDetails {...props} info={pokemons}/> }/>
       </Switch>
     </BrowserRouter>
